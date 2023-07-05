52637e4e598bc6a4d26f15a476af7b9e4122fbd8
Andrea Creazzi <andreacreazzi@libero.it>
2023-07-05 12:01:30 +0200

    add AppHeader + AppMain

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
+  components: { AppHeader, AppMain },
   data() {
     return {}
   }
 }
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
 
 <style></style>
