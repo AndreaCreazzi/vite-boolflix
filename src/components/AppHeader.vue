93aa09bafaa5ee0a680025df57bbe291cf47ab0c
Andrea Creazzi <andreacreazzi@libero.it>
2023-07-06 17:54:45 +0200

    fix AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,17 +10,19 @@ export default {
 </script>
 
 <template>
-    <nav class="container-fluid py-3">
-        <div class="row">
-            <div class="col-6 d-flex align-items-center">
-                <h1>BOOLFLIX</h1>
-            </div>
-            <div class="col-6 d-flex align-items-center">
-                <form class="input-group justify-content-end" @submit.prevent="$emit('form-submit')">
-                    <input v-model.trim="selectFilter" @keyup="$emit('term-change', selectFilter)" type="text"
-                        placeholder="Film oppure Serie TV">
-                    <button class="btn btn-secondary" type="submit">Cerca</button>
-                </form>
+    <nav class="py-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 d-flex align-items-center">
+                    <h1>BOOLFLIX</h1>
+                </div>
+                <div class="col-6 d-flex align-items-center">
+                    <form class="input-group justify-content-end" @submit.prevent="$emit('form-submit')">
+                        <input v-model.trim="selectFilter" @keyup="$emit('term-change', selectFilter)" type="text"
+                            placeholder="Film oppure Serie TV">
+                        <button class="btn btn-secondary" type="submit">Cerca</button>
+                    </form>
+                </div>
             </div>
         </div>
     </nav>
